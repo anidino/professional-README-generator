d617d487d726e3a4b0357295308aeb854e18ef9d
AniElaina <anicook13@gmail.com>
2021-07-15 17:25:34 -0400

    questions are set up

---
+++ Develop/index.js
@@ -0,0 +1,103 @@
+
+// TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
+// const generateMarkdown = require("./utils/generateMarkdown")
+
+//create variable where the file will be written to and copied. Must have relative file path name
+const { writeFile, copyFile } = require('./utils/generateMarkdown');
+
+
+// TODO: Create an array of questions for user input (starter code)
+var questionsPrompt = () => {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is the name of your project? (Required)",
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log("You must enter the name of your project.");
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Please describe your project (Required)",
+            validate: descriptionInput => {
+                if (descriptionInput) {
+                    return true;
+                } else {
+                    console.log("You must enter a brief description of your project.");
+                    return false;
+                }
+            }
+        },
+        {
+            type: "confirm",
+            name: "confirmTable",
+            message: "Would you like to have a table of contents (for longer README files)?",
+            default: false  // do i put the if/else statement after this or can it be at the end? 
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "What is the installation process, if any, for your project? (Optional)"
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "Please provide instructions for use. (Required)",
+            validate: usageInput => {
+                if (usageInput) {
+                    return true;
+                } else {
+                    console.log("You must provide basic instructions for use.");
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "contributing",
+            message: "If you would like other developers to contribute to your project, please add guidelines for how to do so. (Optional)"
+        },
+        {
+            type: "input",
+            name: "test",
+            message: "What tests, if any, have you written for your application? (Optional)"
+        }
+    ])
+};
+
+questionsPrompt();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) { }  //starter code
+
+// TODO: Create a function to initialize app
+function init() { }                      //starter code
+
+// Function call to initialize app
+init();                                //starter code
---
+++ Develop/utils/generateMarkdown.js
@@ -0,0 +1,21 @@
+
+const fs = require("fs");
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) { }                         //starter code
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) { }                         //starter code
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) { }                           //starter code
+
+// TODO: Create a function to generate markdown for README     //starter code below
+function generateMarkdown(data) {
+    return `# ${data.title}                                      
+`;
+}
+
+module.exports = generateMarkdown;
